cba7c0382f4067f9b21f50572d810ef26ec41ddb
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-06-24 21:32:08 +0530

    h

--- index.js
+++ index.js
@@ -12,9 +12,16 @@ app.use(express.static("public"));
 
 // 4. When the user goes to the home page it should render the index.ejs file.
 app.get("/", async (req, res) => {
-    const result = await axios.get("https://secrets-api.appbrewery.com/random");
-    res.render("index.ejs", { secret: result.data.secret, user: result.data.username });
-})
+    try {
+        const result = await axios.get("https://secrets-api.appbrewery.com/random");
+        res.render("index.ejs", { secret: result.data.secret, user: result.data.username });
+    }
+    catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+
+});
 
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
