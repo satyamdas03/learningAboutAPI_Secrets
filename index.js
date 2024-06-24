46d9ce99f08078ef426e253aa4cbef59b1261560
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-06-24 21:18:35 +0530

    g

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ const app = express();
 const port = 3000;
 
 // 3. Use the public folder for static files.
+app.use(express.static("public"));
 
 // 4. When the user goes to the home page it should render the index.ejs file.
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
 
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
