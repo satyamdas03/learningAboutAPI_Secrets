0029ce04c4cd8a53158ed47ea62acfe16c798a1f
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-06-24 21:17:02 +0530

    new

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // HINTS:
 // 1. Import express and axios
+import express from "express";
+import axios from "axios";
 
 // 2. Create an express app and set the port number.
+const app = express();
+const port = 3000;
 
 // 3. Use the public folder for static files.
 
